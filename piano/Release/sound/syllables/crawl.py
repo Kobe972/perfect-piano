30f89372c6c81227478119bdc54d4f23ccca5460
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2021-09-12 16:25:32 +0800

    Add files via upload

--- piano/Release/sound/syllables/crawl.py
+++ piano/Release/sound/syllables/crawl.py
@@ -1,7 +1,7 @@
 import requests
 session=requests.Session()
 for i in range(0,7):
-    for j in ['C','D','E','F','G','A','B']:
+    for j in ['Cs','Ds','Fs','Gs','As']:
         url='https://cdn.jsdelivr.net/gh/warpprism/cdn@latest/autopiano/static/samples/bright_piano/'+j+str(i).lower()+'.mp3'
         with open(j+str(i)+'.mp3','wb') as fd:
             fd.write(session.get(url).content)
